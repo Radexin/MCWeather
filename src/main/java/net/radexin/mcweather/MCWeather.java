626023d3c27687faf1598fdda7b83e8686f16c76
Mikhail Gorulko <radexinrblx@gmail.com>
2023-07-05 22:16:14 +0300

    tick handler + check for mod

--- src/main/java/net/radexin/mcweather/MCWeather.java
+++ src/main/java/net/radexin/mcweather/MCWeather.java
@@ -1,10 +1,16 @@
 package net.radexin.mcweather;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.CustomizeGuiOverlayEvent;
+import net.minecraftforge.common.ForgeMod;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -13,11 +19,15 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+import java.awt.*;
+
 @Mod(MCWeather.MOD_ID)
 public class MCWeather
 {
     public static final String MOD_ID = "mcweather";
     private static final Logger LOGGER = LogUtils.getLogger();
+    private static int tickCounter = 0;
+
     public MCWeather()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -31,9 +41,28 @@ public class MCWeather
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
-    }
+        // Check for terranew mod which is required for calculating position
+        /*
+        if (ModList.get().isLoaded("teranew"))
+        {
 
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+        }
+        */
+    }
+    @Mod.EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT)
+    public static class ClientForgeEvents
+    {
+        @SubscribeEvent
+        public static void onTick(TickEvent.ClientTickEvent event)
+        {
+            if (event.phase == TickEvent.Phase.END) {
+                tickCounter++;
+                if (Minecraft.getInstance().player != null) {
+                }
+            }
+        }
+    }
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientModEvents
     {
         @SubscribeEvent
